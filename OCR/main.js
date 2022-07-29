ddf8fec53cc7773fb57eabd1a78e04049a977292
Pratik Kumar <kpratik1929@gmail.com>
2022-07-29 11:42:04 +0530

    updated Dashboard

---
+++ OCR/main.js
@@ -0,0 +1,6 @@
+var a = document.getElementById('upload_image');
+var b = document.getElementById('upload_file');
+function image_upload() {
+    console.log('clicked');
+    document.getElementById('upload_file').click()
+}
